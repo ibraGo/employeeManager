a48c6d24431a4edfc6eb4d89765e4941a0db3c00
ibra15 <iabdeneby@gmail.com>
2018-11-08 17:18:04 +0100

    configure routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListComponent } from './components/list/list.component';
+import { ShowComponent } from './components/show/show.component';
+import { FormComponent } from './components/form/form.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'login', component: LoginComponent},
+  { path:'list', component: ListComponent},
+  { path:'show', component: ShowComponent},
+  { path:'form', component: FormComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
